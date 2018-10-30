249ff7903ecbdbcf1bb863725d01c6cbbe09af63
sagie gur ari <sagiegurari@gmail.com>
2018-10-30 14:48:37 +0200

    update tests

--- tests/testthat/test_generate_env_setup_script.R
+++ tests/testthat/test_generate_env_setup_script.R
@@ -11,14 +11,14 @@ describe("generate_env_setup_script", {
     })
 
     it("multiple env vars", {
-        script <- scriptexec::generate_env_setup_script(c("ENV1=v1", "ENV2=v2"))
+        script <- scriptexec::generate_env_setup_script(c("e1=v1", "e2=v2"))
 
         length <- length(script)
         expect_equal(length, 2)
 
         prefix <- get_os_string("export", "SET")
-        expected_result <- c(paste(prefix, "ENV1=v1", sep = " "), paste(prefix,
-            "ENV2=v2", sep = " "))
+        expected_result <- c(paste(prefix, "e1=v1", sep = " "), paste(prefix,
+            "e2=v2", sep = " "))
 
         expect_equal(expected_result, script)
     })
